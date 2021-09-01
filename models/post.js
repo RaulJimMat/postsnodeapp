38015692df3e35bc8d2cc2ec2d58c26a313d64d7
RaulJimMat <68312996+cmtCompgdt@users.noreply.github.com>
2021-08-31 21:47:38 -0500

    Add code for SHOW Post

--- models/post.js
+++ models/post.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
   title: String,
-  prince: String,
+  price: String,
   description: String,
   images:  [ String ],
   location: String,
