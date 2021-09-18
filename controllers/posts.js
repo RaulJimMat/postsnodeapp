f19a76c9642dab0e2ccc142cc68fbede43896d0d
RaulJimMat <68312996+cmtCompgdt@users.noreply.github.com>
2021-09-18 09:00:28 -0500

    Create and list reviews feature

--- controllers/posts.js
+++ controllers/posts.js
@@ -39,7 +39,10 @@ module.exports = {
   },
 
   async postShow(req,res,next){
-    let post = await Post.findById(req.params.id);
+    let post = await Post.findById(req.params.id).populate({
+      path: 'reviews',
+      options: {sort: {'_id': -1} }
+    });
     res.render('posts/show', { post });
   },
 
--- controllers/reviews.js
+++ controllers/reviews.js
@@ -4,7 +4,12 @@ const Review = require('../models/review');
 module.exports = {
 
   async reviewCreate(req,res,next){
-    
+    let post = await Post.findById(req.params.id);   
+    let review = await Review.create(req.body.review);
+    post.reviews.push(review);
+    post.save();
+    req.session.success = 'New review created!';
+    res.redirect(`/posts/${post.id}`);
   },
 
   async reviewUpdate(req,res,next){
