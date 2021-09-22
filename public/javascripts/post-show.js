870ac1682cc86363273ada1cc446e4c812815cfa
RaulJimMat <68312996+cmtCompgdt@users.noreply.github.com>
2021-09-21 21:24:38 -0500

    Add a toggeable form to edit a review in the show post view

--- public/javascripts/post-show.js
+++ public/javascripts/post-show.js
@@ -17,3 +17,12 @@ new mapboxgl.Marker(el)
 .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
 .setHTML('<h3>' + post.title + '</h3><p>' + post.location + '</p>'))
 .addTo(map);
+
+//Toggle Edit review form
+$('.toggle-edit-form').on('click', function(){
+//toggle the edit button text on click
+$(this).text() === 'Edit' ? $(this).text('Cancel') : $(this).text('Edit');
+//toggle the visibility of the edit review form
+$(this).siblings('.edit-review-form').toggle();
+	 });
+
